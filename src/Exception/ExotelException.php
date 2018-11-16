5ccce3b0ef6b7a08a73ff8fff7a867c5c6e12e48
sathish <sathish.thi@gmail.com>
2018-11-16 05:39:49 +0000

    Apply fixes from StyleCI

--- src/Exception/ExotelException.php
+++ src/Exception/ExotelException.php
@@ -5,23 +5,21 @@
  *  *  * Unauthorized copying of this file, via any medium is strictly prohibited
  *  *  * Proprietary and confidential
  *  *  * Written by Sathish Kumar(satz) <sathish.thi@gmail.com>ManiKandan<smanikandanit@gmail.com >
- *  *
- *
+ *  *.
  */
 
 /**
  * Created by PhpStorm.
  * User: satz
  * Date: 15/11/18
- * Time: 10:00 PM
+ * Time: 10:00 PM.
  */
 
 namespace Ssatz\ExotelSms\Exception;
 
-
 use Illuminate\Support\Facades\Log;
 
-class ExotelException extends  \Exception
+class ExotelException extends \Exception
 {
     /**
      * Report or log an exception.
